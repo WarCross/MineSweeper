573506d99e8fd8fae07534c87e5380b59567f583
Илья <fallout.0000@mail.ru>
2025-04-10 16:52:57 +0500

    html

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-const repoName = '/MineSweeper/' // Замените на имя своего репозитория
-
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
   resolve: {
@@ -12,29 +12,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: process.env.NODE_ENV === 'production' ? repoName : '/', // Условная настройка base
-  define: {
-    'import.meta.env.BASE_URL': JSON.stringify(
-      process.env.NODE_ENV === 'production' ? repoName : '/',
-    ), // Условная настройка BASE_URL
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          let extType = assetInfo.name.split('.').at(1)
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            extType = 'img'
-          }
-          return `assets/${extType}/[name]-[hash][extname]`
-        },
-        chunkFileNames: 'assets/js/[name]-[hash].js',
-        entryFileNames: 'assets/js/[name]-[hash].js',
-      },
-    },
-  },
-  publicDir: 'public',
+  base: '/MineSweeper/',
 })
