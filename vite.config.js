bfdf48c0775e3da353d440f018d1839d8f813082
Илья <fallout.0000@mail.ru>
2025-04-10 16:58:24 +0500

    commit1

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: '/MineSweeper/',
+  base: '/MineSweeper/', //  Замените "minesweeper-vue" на имя своего репозитория!
 })
